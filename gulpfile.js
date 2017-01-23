aada6c12f5f9cbaf7d2733c74b36332db033310c
vigzmv <vigzmv@outlook.com>
2017-01-24 00:27:16 +0530

    +AutoPrefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,53 @@
 var gulp = require('gulp');
-var webpack = require('webpack');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var pug = require('gulp-pug');
 var reload = browserSync.reload;
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
-var stripAnsi            = require('strip-ansi');
+var stripAnsi = require('strip-ansi');
 var webpackConfig = require('./webpack.config');
-var bundler       = webpack(webpackConfig);
-
+var bundler = webpack(webpackConfig);
 
 var src = {
     sass: 'src/sass/*.sass',
-    css:  'docs/css',
+    css: 'docs/css',
     pug: 'src/*.pug',
     html: 'docs/*.html'
 };
 
-bundler.plugin('done', function (stats) {
-    if (stats.hasErrors() || stats.hasWarnings()) {
-        return browserSync.sockets.emit('fullscreen:message', {
-            title: "Webpack Error:",
-            body:  stripAnsi(stats.toString()),
-            timeout: 100000
-        });
-    }
-    browserSync.reload();
-});
-
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', function() {
     browserSync.init({
         server: './docs',
+        open: false,
         logFileChanges: false,
-        middleware: [
-            webpackDevMiddleware(bundler, {
+        middleware: [webpackDevMiddleware(bundler, {
                 publicPath: webpackConfig.output.publicPath,
-                stats: {colors: true}
-            })
-        ],
-        plugins: ['bs-fullscreen-message'],
+                stats: {
+                    colors: true
+                }
+            })],
+        plugins: ['bs-fullscreen-message']
     });
 
     gulp.watch(src.sass, ['sass']);
     gulp.watch(src.pug, ['pug-watch']);
+    gulp.watch('docs/css/*.css', ['auto-prefix']);
     gulp.watch(src.html).on('change', reload);
 });
 
 gulp.task('sass', function() {
     return gulp.src(src.sass)
-        .pipe(sass())
-        .pipe(gulp.dest(src.css))
-        .pipe(reload({stream: true}));
+    .pipe(sass())
+    .pipe(gulp.dest(src.css))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('auto-prefix', function() {
+    gulp.src('docs/css/*.css')
+    .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+    .pipe(gulp.dest('docs/css'))
 });
 
 gulp.task('templates', function() {
@@ -60,12 +57,20 @@ gulp.task('templates', function() {
     };
 
     return gulp.src(src.pug)
-        .pipe(pug({
-            locals: YOUR_LOCALS
-        }))
-        .pipe(gulp.dest('./docs/'));
+    .pipe(pug({locals: YOUR_LOCALS}))
+    .pipe(gulp.dest('./docs/'));
+});
+
+bundler.plugin('done', function(stats) {
+    if (stats.hasErrors() || stats.hasWarnings()) {
+        return browserSync.sockets.emit('fullscreen:message', {
+            title: "Webpack Error:",
+            body: stripAnsi(stats.toString()),
+            timeout: 100000
+        });
+    }
+    browserSync.reload();
 });
 
 gulp.task('pug-watch', ['templates'], reload);
-
 gulp.task('default', ['serve']);
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,39 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  debug: true,
-  devtool: '#eval-source-map',
+    debug: true,
+    devtool: '#eval-source-map',
 
-  context: path.join(__dirname, 'src', 'js'),
+    context: path.join(__dirname, 'src', 'js'),
 
-  entry: [
-    './main'
-  ],
+    entry: ['./main'],
 
-  output: {
-    path: path.join(__dirname, 'docs','js'),
-    publicPath: '/js/',
-    filename: 'bundle.js'
-  },
+    output: {
+        path: path.join(__dirname, 'docs', 'js'),
+        publicPath: '/js/',
+        filename: 'bundle.js'
+    },
 
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(), new webpack.NoErrorsPlugin()
+    ],
 
-  module: {
-    loaders: [
-      {
-        loader: "babel-loader",
+    module: {
+        loaders: [
+            {
+                loader: "babel-loader",
 
-        // Only run `.js` and `.jsx` files through Babel
-        test: /\.jsx?$/,
+                // Only run `.js` and `.jsx` files through Babel
+                test: /\.jsx?$/,
 
-        exclude: /node_modules/,
+                exclude: /node_modules/,
 
-        // Options to configure babel with
-        query: {
-          plugins: ['transform-runtime'],
-          presets: ['es2015', 'react'],
-        }
-      },
-    ]
-  }
+                // Options to configure babel with
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
 };
